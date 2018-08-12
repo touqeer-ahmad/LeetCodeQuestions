9486beb7b9b3e6255bd430fd82c000c739d07107
Touqeer Ahmad <touqeer.ahmad@outlook.com>
2018-08-11 18:48:13 -0700

    Update 4.-MedianofTwoSortedArrays.scala

--- scala/4.-MedianofTwoSortedArrays.scala
+++ scala/4.-MedianofTwoSortedArrays.scala
@@ -3,12 +3,21 @@
 Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
 
 You may assume nums1 and nums2 cannot be both empty.
-Given Example:
 
-nums1 = [1, 5]
-nums2 = [3]
+Example 1:
 
-The median is 3.0
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+
+https://leetcode.com/problems/median-of-two-sorted-arrays/description/
 */
 
 object Solution {
